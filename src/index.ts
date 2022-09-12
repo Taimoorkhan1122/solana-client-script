385788ebdc3bd88c4d34e094d01d284986fd867c
taimoorkhan1122 <khant8k83@gmail.com>
2022-09-12 16:01:39 +0500

    airdrop sol function

--- src/index.ts
+++ src/index.ts
@@ -4,40 +4,44 @@ import Dotenv from "dotenv";
 Dotenv.config();
 
 // initialize keypair
-async function initializeKeypair(connection: web3.Connection) {
-    const privateKey = !process.env.PRIVATE_KEY;
+async function initializeKeypair(connection: web3.Connection): Promise<web3.Keypair> {
+    const privateKey = process.env.PRIVATE_KEY;
     if (!privateKey) {
         console.log("creating .env fiel ...");
         const signer = web3.Keypair.generate();
-        fs.writeFileSync(
-            ".env",
-            `PRIVATE_KEY=[${signer.secretKey.toString()}]`,
-        );
-
+        fs.writeFileSync(".env", `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
         // airdrop some sol
-        let balance = await connection.getBalance(signer.publicKey);
-        console.log("current balance ", balance / web3.LAMPORTS_PER_SOL);
+        airdropSol(signer, connection);
+        return signer;
+    }
 
-        if (balance / web3.LAMPORTS_PER_SOL < 1) {
-            console.log("airdropping 1 sol ...");
-            const airdropSign = await connection.requestAirdrop(
-                signer.publicKey,
-                web3.LAMPORTS_PER_SOL,
-            );
+    const secret = JSON.parse(privateKey ?? "") as number[];
+    const keypairFromSecret = web3.Keypair.fromSecretKey(Uint8Array.from(secret));
+    //airdrop
+    await airdropSol(keypairFromSecret, connection);
+    return keypairFromSecret;
+}
 
-            const latestBlockHash = await connection.getLatestBlockhash();
-            await connection.confirmTransaction({
-                blockhash: latestBlockHash.blockhash,
-                lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-                signature: airdropSign,
-            });
+// airdrop sol
+async function airdropSol(signer: web3.Keypair, connection: web3.Connection) {
+    let balance = await connection.getBalance(signer.publicKey);
+    console.log("current balance ", balance / web3.LAMPORTS_PER_SOL);
 
-            balance = await connection.getBalance(signer.publicKey);
-            console.log("new balance ", balance / web3.LAMPORTS_PER_SOL);
-        }
+    if (balance / web3.LAMPORTS_PER_SOL < 1) {
+        console.log("airdropping 1 sol ...");
+        const airdropSign = await connection.requestAirdrop(signer.publicKey, web3.LAMPORTS_PER_SOL);
+
+        const latestBlockHash = await connection.getLatestBlockhash();
+        await connection.confirmTransaction({
+            blockhash: latestBlockHash.blockhash,
+            lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+            signature: airdropSign,
+        });
+
+        balance = await connection.getBalance(signer.publicKey);
+        console.log("new balance ", balance / web3.LAMPORTS_PER_SOL);
     }
 }
-// airdrop sol
 // create transfer function
 
 async function main() {}
